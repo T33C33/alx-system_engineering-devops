a981a6e51cc1aa8c0ab3bc16418e8de5de7123a5
T33C33 <teeceeiheukwumere@gmail.com>
2024-05-04 16:38:43 +0100

    correction

--- 0x05-processes_and_signals/102-zombie.c
+++ 0x05-processes_and_signals/102-zombie.c
@@ -5,35 +5,36 @@
 
 int infinite_while(void)
 {
-    while (1)
-    {
-        sleep(1);
-    }
-    return (0);
+	while (1)
+	{
+		sleep(1);
+	}
+	return (0);
 }
 
 int main(void)
 {
-    pid_t pid;
-    int i;
 
-    for (i = 0; i < 5; i++)
-    {
-        pid = fork();
-        if (pid < 0)
-        {
-            perror("fork");
-            exit(1);
-        }
-        else if (pid == 0)
-        {
-            exit(0);
-        }
-        else
-        {
-            printf("Zombie process created, PID: %d\n", pid);
-        }
-    }
-    infinite_while();
-    return 0;
+	pid_t pid;
+	int i;
+
+	for (i = 0; i < 5; i++)
+	{
+		pid = fork();
+		if (pid < 0)
+		{
+			perror("fork");
+			exit(1);
+		}
+		else if (pid == 0)
+		{
+			exit(0);
+		}
+		else
+		{
+			printf("Zombie process created, PID: %d\n", pid);
+		}
+	}
+	infinite_while();
+	return 0;
 }
