d3e29941f3f93a9bc92c69d25ce80a587f5da5ad
T33C33 <teeceeiheukwumere@gmail.com>
2024-07-05 16:22:44 +0100

    check

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,4 +1,12 @@
 # Using Puppet to install flask from pip3.
+package { 'python3':
+  ensure => installed,
+}
+
+package { 'python3-pip':
+  ensure => installed,
+}
+
 package {'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
