ec545b0c55381f095de4ad78febf55b27da7f8c0
T33C33 <teeceeiheukwumere@gmail.com>
2024-07-30 00:49:23 +0100

    new

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -12,10 +12,12 @@ if __name__ == '__main__':
     if len(sys.argv) > 1:
         if re.fullmatch(r'\d+', sys.argv[1]):
             employee_id = int(sys.argv[1])
-            employee_info = requests.get('{}/users/{}'.format(REST_API, employee_id)).json()
+            employee_info = requests.get(
+                '{}/users/{}'.format(REST_API, employee_id)).json()
             tasks_info = requests.get('{}/todos'.format(REST_API)).json()
             employee_name = employee_info.get('name')
-            tasks = list(filter(lambda x: x.get('userId') == employee_id, tasks_info))
+            tasks = list(filter(lambda x: x.get('userId') == employee_id,
+                                tasks_info))
             completed_tasks = list(filter(lambda x: x.get('completed'), tasks))
             print(
                 'Employee {} is done with tasks({}/{}):'.format(
@@ -27,4 +29,3 @@ if __name__ == '__main__':
             if len(completed_tasks) > 0:
                 for task in completed_tasks:
                     print('\t {}'.format(task.get('title')))
-                    
---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,22 @@
+import csv
+import requests
+import sys
+
+#!/usr/bin/python3
+""" Export api to csv"""
+
+if __name__ == '__main__':
+    user_id = sys.argv[1]
+    url_user = 'https://jsonplaceholder.typicode.com/users/' + user_id
+    response = requests.get(url_user)
+    user_name = response.json().get('username')
+    task_url = url_user + '/todos'
+    response = requests.get(task_url)
+    tasks = response.json()
+
+    with open('{}.csv'.format(user_id), 'w') as csvfile:
+        for task in tasks:
+            completed = task.get('completed')
+            title = task.get('title')
+            csvfile.write('"{}","{}","{}","{}"\n'.format(
+                user_id, user_name, completed, title))
