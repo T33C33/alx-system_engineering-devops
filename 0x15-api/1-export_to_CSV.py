8da472e05c7be9415cf51b784879e8410509f7fb
T33C33 <teeceeiheukwumere@gmail.com>
2024-07-30 00:51:24 +0100

    new

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,9 +1,8 @@
+#!/usr/bin/python3
+"""Export data in the CSV format"""
 import csv
 import requests
 import sys
-
-#!/usr/bin/python3
-""" Export api to csv"""
 
 if __name__ == '__main__':
     user_id = sys.argv[1]
