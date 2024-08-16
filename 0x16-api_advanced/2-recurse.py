49b98549cfe5e7e7c4ef126a46fcc35cae2088e9
T33C33 <teeceeiheukwumere@gmail.com>
2024-08-16 01:33:54 +0100

    new

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,14 +1,9 @@
 #!/usr/bin/python3
 import requests
 
-
 """
 A recursive function that
 queries the Reddit API
-and returns a list containing the titles
-of all hot articles for a given subreddit.
-If no results are found for the given subreddit,
-the function should return None.
 """
 
 
